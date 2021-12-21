fe648eacf76908e6a259334cefd9e299f9252fb0
nanplouf <vera78ds@gmail.com>
2021-12-21 10:19:35 +0100

    Création du dossier css du dossier img, avec les img du projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Projet PHP Page test</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="asset/css/script.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    
+</body>
+</html>
