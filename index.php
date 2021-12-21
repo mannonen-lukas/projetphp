1609c282bffd47cf1dfe32f1fffed9e498b7208e
nanplouf <vera78ds@gmail.com>
2021-12-21 15:58:39 +0100

    Visualisation des page avec la fonction pageXml et création de la fonction pour les boutons de la navbar

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+function navBtn($numberPage){
+    $fichier = 'source.xml';
+    $xml = simplexml_load_file($fichier);
+    echo '<a href="">' . $xml->page[$numberPage]->menu . '</a>';
+};
+function pageXml($numberPage){
+    $fichier = 'source.xml';
+    $xml = simplexml_load_file($fichier);
+    echo '<h1>' . $xml->page[$numberPage]->title . '</h1>';
+    echo $xml->page[$numberPage]->content;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
@@ -7,6 +20,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    
+<nav><?php 
+    navBtn(0);
+    navBtn(1);
+    navBtn(2);
+    navBtn(3);
+    ?></nav>
+    <?php pageXml(3)?>
 </body>
 </html>
