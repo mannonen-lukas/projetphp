7c25ad8279204bb30ef80192add992ddc6a94727
nanplouf <vera78ds@gmail.com>
2021-12-22 13:58:01 +0100

    Mise à jour du css

--- index.php
+++ index.php
@@ -9,50 +9,46 @@ require('assets/lib/source.php');
     <title>Projet PHP Page test</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="asset/css/script.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/script.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-
 <body>
-
     <header>
         <nav class="myNav">
             <div class="row">
+                <div class="col m-1">
+                    <img src="/projetphp/assets/img/logo.png" alt="Logo" width="80px">
+                </div>
                 <!--Menu mobile-->
-                <div class="col dropdown d-block d-md-block d-lg-block d-xl-none d-xxl-none">
+                <div class="col-2 dropdown d-block d-md-block d-lg-block d-xl-none d-xxl-none">
                     <i class="fas fa-bars iBars m-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></i>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" type="button" href="#">Accueil</a></li>
-                        <li><a class="dropdown-item" type="button" href="#">Qui sommes-nous?</a></li>
-                        <li><a class="dropdown-item" type="button" href="#">Nos clients témoignent</a></li>
-                        <li><a class="dropdown-item" type="button" href="#">Contact</a></li>
+                        <li><?= menuMobil($menu) ?></li>
                     </ul>
                 </div>
                 <!--Menu WEB-->
-                <div class="col-2 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
-                </div>
-                <div class="col-2 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
-                    <button id="welcome" class="navButton">Accueil</button>
-                </div>
-                <div class=" col-2 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
-                    <button id="us" class="navButton">Qui sommes-nous?</button>
-                </div>
-                <div class="col-2 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
-                    <button id="customers" class="navButton">Nos clients témoignent</button>
-                </div>
-                <div class="col-2 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
-                    <button id="contact" class="navButton">Contact</button>
+                <div class="col-9 mt-3 text-align-center d-none d-md-none d-lg-none d-xl-block d-xxl-block">
+                    <?= menuWeb($menu) ?>
                 </div>
             </div>
         </nav>
     </header>
-
-<?php
-    echo renderMenu($menu);
-    echo renderPage($content);
-?>
+    <div class="myBody">
+        <!-- ici le contenue de la page-->
+        <?= renderPage($content);?>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col">Maçonnerie Ocordo</div>
+                <div class="col">02.51.84.18.24</div>
+                <div class="col">contact@ocordo-travaux.fr</div>
+            </div>
+        </div>
+    </footer>
+<!-- JavaScript pour la nav bar mobile -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
-
 </html>
